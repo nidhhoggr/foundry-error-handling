094e232479dc1897a1a3eeba45187873cc3c8cde
Nidhhoggr <persie.joseph@gmail.com>
2022-12-19 18:33:40 +0000

    getting test working

--- src/ErrorThrowingContract.sol
+++ src/ErrorThrowingContract.sol
@@ -1,19 +1,34 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
-contract ErrorThrowingContract {
-    error CustomError();
-    error CustomErrorFrom(uint8 _from);
+error CustomError();
+//from paramater alleviates the need to create two seperate errors
+//while still explaining a particular error e.g. MaxTokenAllotment
+//and can allow test to determine where the error was triggered
+error CustomErrorFrom(uint _from);
 
-    function throwCustomError() public {
+contract ErrorThrowingContract {
+    
+    function throwsCustomError() public pure {
         revert CustomError();
     }
 
-    function throwsCustomErrorFromOne() public {
+    function throwsCustomErrorFromOne() public pure {
         revert CustomErrorFrom(1);
     }
 
-    function throwsCustomErrorFromTwo() public {
+    function throwsCustomErrorFromTwo() public pure {
         revert CustomErrorFrom(2);
+    }
+
+    //illustartes a use case where the error paramater becomes useful
+    //for testing purposes using static values as parameters
+    function throwsCustomErrorFromParam(uint param) public pure {
+        if (param > 10) {
+            revert CustomErrorFrom(1);
+        }
+        else {
+            revert CustomErrorFrom(2);
+        }
     }
 }
--- test/ETCTesting.t.sol
+++ test/ETCTesting.t.sol
@@ -1,29 +1,74 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
-import "forge-std/Test.sol";
+import {DSTest} from "ds-test/test.sol";
 import {Vm} from "forge-std/Vm.sol";
-import "../src/ErrorThrowingContract.sol";
+import {ErrorThrowingContract,CustomError,CustomErrorFrom} from "../src/ErrorThrowingContract.sol";
 
-contract ETCTesting is Test {
-    ErrorThrowingContract public etcInstance;
+contract ETCTesting is DSTest {
+    ErrorThrowingContract internal etcInstance;
+    Vm internal immutable vm = Vm(HEVM_ADDRESS);
 
     function setUp() public {
         etcInstance = new ErrorThrowingContract();
     }
 
+    function assertExpectedSelector(bytes memory reason, bytes32 mSelector) internal {
+        bytes4 selector = bytes4(reason);
+        assertEq(selector, mSelector);
+    }
+
+    function _shiftFourBytes(bytes memory reason) internal pure returns (bytes memory data) {
+        assembly {data := mload(add(reason, 36))}
+    }
+
+    function assertExpectedArgument(bytes memory reason, bytes32 argument) internal {
+        bytes32 parsed; 
+        assembly {parsed := mload(add(reason, 36))}
+        assertEq(argument, parsed);
+    }
+
     function testExpectCustomError() public {
-        vm.
+        vm.expectRevert(CustomError.selector);
+        etcInstance.throwsCustomError();
+    }
+
+    function testFailExpectCustomErrorFromOne() public {
+        //fails because the argument calldata expected in revert
+        //[FAIL. Reason: Error != expected error: 0x25a145180000000000000000000000000000000000000000000000000000000000000001 != 0x25a14518]
+        vm.expectRevert(CustomErrorFrom.selector);
         etcInstance.throwsCustomErrorFromOne();
     }
 
     function testExpectCustomErrorFromOne() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
+        try etcInstance.throwsCustomErrorFromOne() {}
+        catch(bytes memory reason) {
+            assertExpectedSelector(reason, CustomErrorFrom.selector);
+            assertExpectedArgument(reason, bytes32(uint(1)));
+        }
     }
 
-    function testExpectCustomErrorFromOneUsingAbiDecode() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
+    function testExpectCustomErrorFromTwo() public {
+        try etcInstance.throwsCustomErrorFromTwo() {}
+        catch(bytes memory reason) {
+            assertExpectedSelector(reason, CustomErrorFrom.selector);
+            assertExpectedArgument(reason, bytes32(uint(2)));
+        }
+    }
+
+    function testExpectCustomErrorFromParamResultsFromOne() public {
+        try etcInstance.throwsCustomErrorFromParam(11) {}
+        catch(bytes memory reason) {
+            assertExpectedSelector(reason, CustomErrorFrom.selector);
+            assertExpectedArgument(reason, bytes32(uint(1)));
+        }
+    }
+
+    function testExpectCustomErrorFromParamResultsFromTwo() public {
+        try etcInstance.throwsCustomErrorFromParam(5) {}
+        catch(bytes memory reason) {
+            assertExpectedSelector(reason, CustomErrorFrom.selector);
+            assertExpectedArgument(reason, bytes32(uint(2)));
+        }
     }
 }
